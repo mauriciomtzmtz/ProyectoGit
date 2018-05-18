0fc68d097e61ba73f1c871fabc6b13af157fff1d
mauricio martinez <martinezmtz901@gmail.com>
2018-05-18 17:38:31 +0000

    se borro el codigo que se subio

--- app/src/main/java/com/example/mauriciomartinez/proyectogit/MainActivity.java
+++ app/src/main/java/com/example/mauriciomartinez/proyectogit/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //se modifico el archivo para hacer un committ
     }
 }
